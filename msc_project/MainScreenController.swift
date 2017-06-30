b576227bcc1cb711dd0f41146247b26605e7922f
Nihat Ozcan [sc16nbo] <sc16nbo@csmac001.leeds.ac.uk>
2017-06-30 17:52:27 +0100

    Basic Design Initiated

--- msc_project/MainScreenController.swift
+++ msc_project/MainScreenController.swift
@@ -1,5 +1,5 @@
-//
-//  ViewController.swift
+
+//  MainScreenController.swift
 //  msc_project
 //
 //  Created by Nihat Ozcan [sc16nbo] on 30/06/2017.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainScreenController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
